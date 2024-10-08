15c1595c34a721e0e2d24fc5f04404946430aea2
Shubhamze <Shubhamgupta81270@gmail.com>
2024-10-08 15:40:03 +0530

    Pushing_complete_Project

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const connectDB = require('./config/db');
+const cors = require('cors');
+const app = express()
+
+connectDB();
+app.use(cors());
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
